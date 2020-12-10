865fedc2444eb0b6b71183f26a87576895ae737d
Marcio Albano <marcio.ahf@gmail.com>
2020-12-10 11:00:13 -0300

    Add new ISR routine for send data button
    This commit adds a specific interrupt routine for controlling the trigger
    button for sending data. Now, the send_data() routine is periodic (through
    timerOne) and controls the data sampling rate, and the check_button() routine
    triggers another interrupt to activate the send_data flag.

--- CapSensorPD/CapSensorPD.ino
+++ CapSensorPD/CapSensorPD.ino
@@ -3,56 +3,64 @@
 // touch gestures on the triangle. Through touch detection, signals will be sent to Pure Data,
 // triggering sounds.
 
-
 #include <CapacitiveSensor.h>
 #include <TimerOne.h>
 
-#define SEND_PIN 4
-#define RECEIVE_PIN 2
-#define ACTIVATE_BUTTON A0
+#define SEND_PIN 6
+#define RECEIVE_PIN 4
+#define ACTIVATE_BUTTON 3
 #define LED_PIN 8
+#define SAMPLING_PERIOD 50000//period for each TimerOne interrupt (microsseconds)
 
-// Send Pin: 4 
+// Send Pin: 4
 // Receive Pin: 2
 // The Sensor Pin is where the wire is attached to the triangle
-CapacitiveSensor   cs_4_2 = CapacitiveSensor(SEND_PIN,RECEIVE_PIN); 
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(SEND_PIN, RECEIVE_PIN);
 int ButtonStatus = 0;
+int SendFlag = 0;
+int debounceTime = 0;
 
-void setup()                    
+void setup()
 {
-   pinMode(LED_PIN,OUTPUT);
-   pinMode(ACTIVATE_BUTTON,INPUT);
-   Timer1.initialize(150000);
-   Timer1.attachInterrupt(check_button);
-   //cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-   Serial.begin(115200);
+  //pin Configurations
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(ACTIVATE_BUTTON, INPUT_PULLUP);
+
+  //Periodic interrupt to activate send_data ISR routine
+  Timer1.initialize(SAMPLING_PERIOD);
+  Timer1.attachInterrupt(send_data);
+
+  //Button Interrupt
+  attachInterrupt(digitalPinToInterrupt(ACTIVATE_BUTTON), check_button, CHANGE);
+  Serial.begin(115200);
+}
+
+
+void send_data() {
+  //Serial.println("Flag Status: " + String(SendFlag));
+  if (SendFlag) {
+    //long start = millis();
+    long total1 =  cs_4_2.capacitiveSensor(1000);
+    //Serial.println(millis() - start);
+    Serial.println(total1);
+
+  }
+
 }
 
 
 void check_button() {
-  if(analogRead(ACTIVATE_BUTTON))
-    {
-      ButtonStatus = !ButtonStatus;
-      digitalWrite(LED_PIN,!digitalRead(LED_PIN));
-    }
+  //Serial.println("apertou o botao");
+  if (analogRead(ACTIVATE_BUTTON))
+  {
+    SendFlag = !SendFlag;
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+
+  }
 }
 
 
-void loop()                    
+void loop()
 {
-    //Serial.println(ButtonStatus);
-    
-    if(ButtonStatus) {
-  
-    long start = millis();
-    long total1 =  cs_4_2.capacitiveSensor(1000);
 
-    //Serial.println(millis() - start);        // check on performance in milliseconds
-    //Serial.print("\t");                    // tab character for debug windown spacing
-
-    Serial.println(total1);                  // print sensor output 1
-
-    }
-    
-    //delay(10);                             // arbitrary delay to limit data to serial port 
 }
