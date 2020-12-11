af40ebb51678795b28cdf3967df5d298780baa36
Marcio Albano <marcio.ahf@gmail.com>
2020-12-11 01:42:08 -0300

    Send OSC messages using SLIP Encoded Serial
    This commit adds some commands to send data using the CNMAT OSC
    library, with SLIP Encoded Serial. Instead of sending directly
    the data over the serial port, OSC messages will be sent to PureData
    for further processing.

--- CapSensorPD/CapSensorPD.ino
+++ CapSensorPD/CapSensorPD.ino
@@ -6,16 +6,21 @@
 #include <CapacitiveSensor.h>
 #include <TimerOne.h>
 
+#include <OSCMessage.h>
+#include <SLIPEncodedSerial.h>
+
 #define SEND_PIN 6
 #define RECEIVE_PIN 4
 #define ACTIVATE_BUTTON 3
 #define LED_PIN 8
 #define SAMPLING_PERIOD 50000//period for each TimerOne interrupt (microsseconds)
+#define BAUD_RATE 115200
 
 // Send Pin: 4
 // Receive Pin: 2
 // The Sensor Pin is where the wire is attached to the triangle
 CapacitiveSensor   cs_4_2 = CapacitiveSensor(SEND_PIN, RECEIVE_PIN);
+SLIPEncodedSerial SLIPSerial(Serial); // For Sending OSC Messages Over Serial
 int ButtonStatus = 0;
 int SendFlag = 0;
 int debounceTime = 0;
@@ -32,17 +37,30 @@ void setup()
 
   //Button Interrupt
   attachInterrupt(digitalPinToInterrupt(ACTIVATE_BUTTON), check_button, CHANGE);
-  Serial.begin(115200);
+
+  //begin SLIPSerial just like Serial
+  SLIPSerial.begin(BAUD_RATE);
+  Serial.begin(BAUD_RATE);
 }
 
-
+float num;
 void send_data() {
   //Serial.println("Flag Status: " + String(SendFlag));
   if (SendFlag) {
-    //long start = millis();
+    OSCMessage msg("/data");
+    //long start = millis();  //check performance time
     long total1 =  cs_4_2.capacitiveSensor(1000);
-    //Serial.println(millis() - start);
-    Serial.println(total1);
+    
+    //Serial.println(millis() - start); //check performance time
+    //Serial.println(total1); //check data using Serial Plotter, if desired.
+
+    //Send OSC message
+    num = total1;
+    msg.add(num);
+    SLIPSerial.beginPacket();
+    msg.send(SLIPSerial);
+    SLIPSerial.endPacket();
+
 
   }
 
