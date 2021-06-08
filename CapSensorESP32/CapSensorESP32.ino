1fbb5f108938252fd0d8d008b4b037430e51a22e
Marcio Albano <marcio.ahf@gmail.com>
2021-06-08 19:34:55 -0300

    Add Wireless Communication for OSC Messages
    This commit implements a new communication protocol for the .ino sketch,
    allowing two ways for sending the OSC messages to pure data. Now it is
    possible to choose between sending the OSC messages via SLIP encoded serial
    or using UDP packages via WiFi. A WIFIMODE macro is defined for selecting
    between transmition modes.

--- CapSensorESP32/CapSensorESP32.ino
+++ CapSensorESP32/CapSensorESP32.ino
@@ -2,12 +2,13 @@
 // Description: Using Arduino CapSense Library, this code will acquire the thumb finger
 // touch gestures on the triangle. Through touch detection, signals will be sent to Pure Data,
 // triggering sounds.
-
+#include <WiFi.h>
 #include <CapacitiveSensor.h>
 #include <OSCMessage.h>
 #include <SLIPEncodedSerial.h>
 #include <Ticker.h>
 
+#define WIFIMODE 0
 #define SEND_PIN 13
 #define RECEIVE_PIN 12
 #define ACTIVATE_BUTTON 5
@@ -30,6 +31,17 @@ int debounceTime = 0;
 long counter = 0;
 float num;
 
+char ssid[] = "";          // your network SSID (name)
+char pass[] = "";          // your network password
+
+WiFiUDP Udp;
+
+const IPAddress outIp(192, 168, 0, 104);     // remote IP of your computer
+const unsigned int outPort = 9005;          // remote port to receive OSC
+const unsigned int localPort = 9000;        // local port to listen for OSC packets (actually not used for sending)
+
+
+
 Ticker t;
 hw_timer_t * timer = NULL;
 
@@ -40,13 +52,39 @@ void stop_timer(){
 
 void setup()
 {
-  Serial1.begin(BAUD_RATE, SERIAL_8N1, RXD2, TXD2);
+
+#if WIFIMODE == 1
+  Serial.begin(BAUD_RATE);
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(localPort);
+#else
 
   //start_timer();
 
+  Serial1.begin(BAUD_RATE, SERIAL_8N1, RXD2, TXD2);
+
   //begin SLIPSerial just like Serial
   SLIPSerial.begin(BAUD_RATE);
-
+#endif
   digitalWrite(LED_PIN, LOW);
 
   t.attach(0.001,&send_data);
@@ -62,12 +100,17 @@ void loop()
     SendFlag = 0;
     OSCMessage msg("/data");
     //long start = millis();  //check performance time
-    float total1 =  cs_4_2.capacitiveSensor(200);
-
+    float total1 =  cs_4_2.capacitiveSensor(50);
     msg.add(total1);
+#if WIFIMODE == 1
+    Udp.beginPacket(outIp, 9005);
+    msg.send(Udp);
+    Udp.endPacket();
+#else
     SLIPSerial.beginPacket();
     msg.send(SLIPSerial);
     SLIPSerial.endPacket();
+#endif
     msg.empty();
   }
 }
